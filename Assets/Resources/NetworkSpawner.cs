56480f05aa2aa672569f10da6851dd848e64e482
Marcel Zwart <marcel__zwart@hotmail.com>
2016-06-03 12:18:01 +0200

    Network spawner toegevoegd voor het spawnen van de resources. close #25

---
+++ Assets/Resources/NetworkSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSpawner : NetworkBehaviour
+{
+	public GameObject oreDepositPrefab;
+	public List<Vector3> positions = new List<Vector3>() {
+		new Vector3(0, 0, -10)
+	};
+
+	public override void OnStartServer()
+	{
+		foreach(Vector3 position in positions)
+		{
+			Quaternion rotation = Quaternion.Euler(0, 0, 0);
+
+			GameObject oreDeposit = (GameObject)Instantiate(oreDepositPrefab, position, rotation);
+			NetworkServer.Spawn(oreDeposit);
+		}
+	}
+}
