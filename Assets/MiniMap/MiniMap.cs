a9f12b5cb361d2f1725c83f74867cc4ee8e602ef
westhoffken <westhoffken@gmail.com>
2016-06-07 11:23:43 +0200

    Fixed audio listener message

--- Assets/MiniMap/MiniMap.cs
+++ Assets/MiniMap/MiniMap.cs
@@ -5,16 +5,13 @@ using UnityEngine.EventSystems;
 public class MiniMap : MonoBehaviour, IPointerDownHandler {
 
     private Vector3 clickMousePos;
-    private Vector3 clickPos;
+
   
 
     public void OnPointerDown(PointerEventData ped)
     {
         Vector3 localHit = transform.InverseTransformPoint(ped.pressPosition);
-        Debug.Log("Localhit" + localHit);
-        clickPos = new Vector3(transform.position.x + 256, 0, 0);
         clickMousePos = new Vector3((2000 / 256) * localHit.x + 50, Camera.main.transform.position.y, (2000 / 256) * localHit.y - 20);
-
         Camera.main.transform.position = clickMousePos;
     }
 }
