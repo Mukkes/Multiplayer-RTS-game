d256109a83a7231e94a74c5b013ee0627e2d9312
westhoffken <westhoffken@gmail.com>
2016-06-07 12:53:54 +0200

    Camera movement
    Movement can now be done with keys too.

--- Assets/RTS/ResourceManager.cs
+++ Assets/RTS/ResourceManager.cs
@@ -6,11 +6,12 @@ namespace RTS
 {
 	public static class ResourceManager
 	{
-		public static float ScrollSpeed { get { return 25; } }
+		public static float ScrollSpeed { get { return 35; } }
 
+        //rotation is no longer used/allowed
 		public static float RotateAmount { get { return 10; } }
-
 		public static float RotateSpeed { get { return 100; } }
+
 
 		public static int ScrollWidth { get { return 15; } }
 
